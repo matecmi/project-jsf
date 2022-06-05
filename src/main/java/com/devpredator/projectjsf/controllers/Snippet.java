2c58733d1eb7ecdb2de6baf24ce047ba40ff0db2
matecmi <kevin2010_12@hotmail.com>
2022-06-04 22:39:00 -0500

    hollaaaaa

---
+++ src/main/java/com/devpredator/projectjsf/controllers/Snippet.java
@@ -0,0 +1,7 @@
+package com.devpredator.projectjsf.controllers;
+
+public class Snippet {
+	$ git commit -m "Nombre del proyecto: projecto-jsf version 1.1 Descripción: Se crea el login del aplicativo y se redirecciona a la pantalla principal
+	
+}
+
